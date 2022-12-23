ff7211a362a341e6861c51fd6c30bad3eec6ff74
AlfiS.R <alfisyahrin673@gmail.com>
2022-12-23 13:39:20 +0700

    Memperbaiki tampilan login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,8 @@
         <ul>
             <li><a class="{{ $active === 'home' ? 'active' : '' }}" href="/">Home</a></li>
             <li><a class="{{ $active === 'about' ? 'active' : '' }}" href="/about">About</a></li>
-            <li><a class="{{ $active === 'rooftop' ? 'active' : '' }}" href=".rooftop">Rooftoop Moments</a></li>
-            <li><a class="{{ $active === 'rooftop' ? 'active' : '' }}" href="/menu">Menu Fav</a></li>
+            <li><a class="{{ $active === 'rooftop' ? 'active' : '' }}" href="/rooftop">Rooftoop Moments</a></li>
+            <li><a class="{{ $active === 'menu' ? 'active' : '' }}" href="/menu">Menu Fav</a></li>
             <li><a class="{{ $active === 'contact' ? 'active' : '' }}" href="/contact">Contact Us</a></li>
             <li><a class="{{ $active === 'login' ? 'active' : '' }}" href="/login">Login</a></li>
         </ul>
@@ -17,28 +17,31 @@
 
     <div class="content">
         <div class="textBox">
-        <h2>Restaurant<br><span>Affomato</span></h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, s
-            ed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris n
-             isi ut aliquip ex ea commodo.</p>
-        <a href="#">View Menu</a>
-    </div>
+            <h2>Restaurant<br><span>Affomato</span></h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, s
+                ed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris n
+                isi ut aliquip ex ea commodo.</p>
+            <a href="#">View Menu</a>
+        </div>
 
-    <div class="imgBox">
-        <img src="/assets/img/img1.png" class="starbucks">
-    </div>
+        <div class="imgBox">
+            <img src="/assets/img/img1.png" class="starbucks">
+        </div>
 
     </div>
-        <ul class="thumb">
-            <li><img src="/assets/img/img1.png" onclick="imgSlider('/assets/img/img1.png');changeCircleColor('#ffbe00')"></li>
-            <li><img src="/assets/img/img2.png" onclick="imgSlider('/assets/img/img2.png');changeCircleColor('#90b217')"></li>
-            <li><img src="/assets/img/img3.png" onclick="imgSlider('/assets/img/img3.png');changeCircleColor('#90b217')"></li>
-        </ul>
-        <ul class="sci">
-            <li><a href="#"><img src="/assets/img/f.png"></a></li>
-            <li><a href="#"><img src="/assets/img/i.png"></a></li>
-            <li><a href="#"><img src="/assets/img/t.png"></a></li>
-        </ul>
+    <ul class="thumb">
+        <li><img src="/assets/img/img1.png" onclick="imgSlider('/assets/img/img1.png');changeCircleColor('#ffbe00')">
+        </li>
+        <li><img src="/assets/img/img2.png" onclick="imgSlider('/assets/img/img2.png');changeCircleColor('#90b217')">
+        </li>
+        <li><img src="/assets/img/img3.png" onclick="imgSlider('/assets/img/img3.png');changeCircleColor('#90b217')">
+        </li>
+    </ul>
+    <ul class="sci">
+        <li><a href="#"><img src="/assets/img/f.png"></a></li>
+        <li><a href="#"><img src="/assets/img/i.png"></a></li>
+        <li><a href="#"><img src="/assets/img/t.png"></a></li>
+    </ul>
     </div>
 </section>
